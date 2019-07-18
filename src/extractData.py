86510a25983bf1053065babdb58ea31247d748d0
Ding Ma <ding.ma@mail.mcgill.ca>
2019-07-18 09:30:01 -0400

    able to read all files in directory and transform into one

--- src/extractData.py
+++ src/extractData.py
@@ -1,6 +1,8 @@
 import csv
-import os
 import datetime
+import os
+
+import pandas as pd
 
 NAPS_PB_File = open("NAPS_PB.csv", "r")
 reader = csv.reader(NAPS_PB_File)
@@ -24,7 +26,8 @@ PB_NAPS_dict = dict(zip(EC_Code, NAPS_ID))
 # 50126
 
 startDate = datetime.datetime(2019, 6, 1)
-endDate = datetime.datetime(2019, 6, 5)
+endDate = datetime.datetime(2019, 6, 30)
+
 delta = endDate-startDate
 listofDate = []
 
@@ -35,47 +38,51 @@ stationlst = []
 datelst =[]
 hourlst=[]
 O3lst=[]
-for files in os.listdir("set_of_data/50126/"):
-    for days in listofDate:
-        if files.endswith("50126_" + days + ".csv"):
-            f = open("set_of_data/50126/"+files, "r")
-            print(files)
-            csvFile = list(csv.reader(f))
-            for z in range(len(csvFile)):
-                row = csvFile[z]
-                station = row[0]
-                stationlst.append(station)
-                print(station)
-                date = row[1]
-                datelst.append(date)
-                hour = row[2]
-                hourlst.append(hour)
-                O3 = row[4]
-                O3lst.append(O3)
-                NO2 = row[5]
-                PM25 = row[6]
+NO2lst = []
+PM25lst = []
+stationsLst = open("stations.txt", "r").read().strip().split(",")
 
-    output = open("monthfiles/"+startDate.strftime("%Y%m")+".csv","w+")
-    for s,d,h,o3 in zip(stationlst,datelst,hourlst,O3lst):
-        print(s+","+d+","+h+","+o3+"\n")
-        output.write(s+","+d+","+h+","+o3+"\n")
+for station in stationsLst:
+    stationID = PB_NAPS_dict[station]
+    if os.path.exists("/fs/home/fs1/ords/oth/airq_central/frc002/Data/CAS/Observations/Station/" + stationID) is True:
+        for files in sorted(
+                os.listdir("/fs/home/fs1/ords/oth/airq_central/frc002/Data/CAS/Observations/Station/" + stationID)):
+            for days in listofDate:
+                if files.endswith(stationID + "_" + days + ".csv"):
+                    print(files)
+                    f = open(
+                        "/fs/home/fs1/ords/oth/airq_central/frc002/Data/CAS/Observations/Station/" + stationID + "/" + files,
+                        "r")
+                    csvFile = list(csv.reader(f))
+                    for z in range(len(csvFile)):
+                        row = csvFile[z]
+                        if row != ['station', 'Date', 'UTC', 'AQHI', 'O3', 'NO2', 'PM2.5', 'PM10', 'SO2', 'H2S', 'CO',
+                                   'NO', 'TRS']:
+                            station = row[0]
+                            stationlst.append(station)
+                            date = row[1]
+                            datelst.append(date)
+                            hour = row[2]
+                            hourlst.append(hour)
+                            O3 = row[4]
+                            O3lst.append(O3)
+                            NO2 = row[5]
+                            NO2lst.append(NO2)
+                            PM25 = row[6]
+                            PM25lst.append(PM25)
 
-# stationsLst = open("stations.txt", "r").read().strip().split(",")
-# for station in stationsLst:
-#     stationID = PB_NAPS_dict[station]
-#     if os.path.exists("set_of_data/"+ PB_NAPS_dict[station]) is True:
-#         for files in os.listdir("set_of_data/"+stationID):
-#             for days in listofDate:
-#                 if files.endswith(stationID+"_" + days + ".csv"):
-#                     f = open("set_of_data/"+stationID+"/" + files, "r")
-#                     csvFile = list(csv.reader(f))
-#                     for z in range(len(csvFile)):
-#                         row = csvFile[z]
-#                         station = row[0]
-#                         date = row[1]
-#                         hour = row[2]
-#                         O3 = row[4]
-#                         NO2 = row[5]
-#                         PM25 = row[6]
-#                         print(stationID, date, hour, O3)
+    df = pd.DataFrame(data={"Date(DD/MM/YYYY)": datelst, "Concentration": O3lst, "Hour(UTC)": hourlst})
+    df.to_csv("monthfiles/" + startDate.strftime("%Y%m") + "_" + stationID + "O3.csv", sep=",", index=False)
 
+    df1 = pd.DataFrame(data={"Date(DD/MM/YYYY)": datelst, "Concentration": NO2lst, "Hour(UTC)": hourlst})
+    df1.to_csv("monthfiles/" + startDate.strftime("%Y%m") + "_" + stationID + "NO2.csv", sep=",", index=False)
+
+    df2 = pd.DataFrame(data={"Date(DD/MM/YYYY)": datelst, "Concentration": PM25lst, "Hour(UTC)": hourlst})
+    df2.to_csv("monthfiles/" + startDate.strftime("%Y%m") + "_" + stationID + "PM25.csv", sep=",", index=False)
+
+    stationlst.clear()
+    datelst.clear()
+    hourlst.clear()
+    O3lst.clear()
+    NO2lst.clear()
+    PM25lst.clear()
